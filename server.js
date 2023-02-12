7a7077fe4bdab17f6badc86d36a2052e52c3a647
chang <kennethchangla@gmail.com>
2023-02-12 12:46:10 +0800

    init project

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const WebSocket = require('ws');
+const installSocket = require('./src/socket/install_socket');
+const PORT = 3000
+
+const server = express().listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
+
+const wss = new WebSocket.Server({ server });
+
+function installSocket(wss) {
+    
+}
+installSocket(wss);
