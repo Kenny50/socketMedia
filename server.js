20d67630889b4a8e9fa21a2f5c0f37749d238cb2
chang <kennethchangla@gmail.com>
2023-02-12 15:15:28 +0800

    server static file

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const installSocket = require('./src/socket/install_socket');
 const installFolderWatcher = require('./src/folder_watcher/folder_watcher');
 const PORT = 3000
 
-const server = express().listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
+const app = express();
+app.use(express.static(__dirname + '/assest')); 
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
 })
 
 const wss = new WebSocket.Server({ server });
